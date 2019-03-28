f1fd9c0e19c3345d3fd491a6ad84254e545ea269
real name genij <zgxzgx@gmail.com>
2019-03-27 20:53:54 -0500

    Getters and Setters done

--- ScheduleApp/User.py
+++ ScheduleApp/User.py
@@ -18,79 +18,90 @@ class User:
     def __methodName__(self):
         # Format for stubs
         return 0
-    
+
+    #Default Constructor
     def __init__(self):
         pass
-    
-    def setPermisson(self):
-        pass
 
-    def setUsername(self):
-        pass
-    
-    def setPassword(self):
-        pass
-    
-    def setEmail(self):
-        pass
-    
-    def setFirstName(self):
-        pass
-    
-    def setLastName(self):
-        pass
-    
-    def setContactPhone(self):
-        pass
-    
-    def setOfficePhone(self):
-        pass
-    
-    def setExtension(self):
-        pass
-    
-    def getPermisson(self):
-        pass
+    #Parameterized Constructor
 
-    def getUsername(self):
-        pass
-    
-    def getPassword(self):
-        pass
-    
-    def getEmail(self):
-        pass
-    
-    def getFirstName(self):
-        pass
-    
-    def getLastName(self):
-        pass
-    
-    def getContactPhone(self):
-        pass
-    
-    def getOfficePhone(self):
-        pass
-    
-    def getExtension(self):
-        pass
-    
-    def getDatabaseID(self, value):
-        # Code
-        return 0
 
+    # Create a new User object from a string containing all the fields separated by commas.
+    # The string is formated as: the permission = [],username = "",password = "",databaseID = 0,email = "",firstName = "",lastName = "",contactPhone = "",officePhone = "",extension = ""
+    # Example: ""
     def fromString(self):
         # code
         return 0
-    
+
+    # Returns a string that has all the fields a User has.
+    # Currently meant to function with fromString.
+    # May want an alternative method for printing out the User in a pretty way.
     def toString(self):
         pass
 
+    # Update all of User's fields within the database.
     def updateDataBase(self):
         # code
         return 0
 
+    #Setters
+    def setPermisson(self, permissionList):
+        self.permission=permissionList
 
+    def setUsername(self, username):
+        self.username=username
+    
+    def setPassword(self, password):
+        self.password=password
 
+    def setDatabaseID(self, databaseID):
+        self.databaseID=databaseID
+    
+    def setEmail(self, email):
+        self.email=email
+    
+    def setFirstName(self, firstName):
+        self.firstName=firstName
+    
+    def setLastName(self, lastName):
+        self.lastName=lastName
+    
+    def setContactPhone(self, contactPhone):
+        self.contactPhone=contactPhone
+    
+    def setOfficePhone(self, officePhone):
+        self.officePhone=officePhone
+    
+    def setExtension(self, extension):
+        self.extension=extension
 
+    #Getters
+    def getPermisson(self):
+        return self.permission
+
+    def getUsername(self):
+        return self.username
+    
+    def getPassword(self):
+        return self.password
+
+    def getDatabaseID(self):
+        return self.databaseID
+    
+    def getEmail(self):
+        return self.email
+    
+    def getFirstName(self):
+        return self.firstName
+    
+    def getLastName(self):
+        return self.lastName
+    
+    def getContactPhone(self):
+        return self.contactPhone
+    
+    def getOfficePhone(self):
+        return self.officePhone
+    
+    def getExtension(self):
+        return self.extension
