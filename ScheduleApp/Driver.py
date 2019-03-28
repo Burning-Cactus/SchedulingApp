d61bba0b22ab0ef779c6acc6192078eae14183a2
Archie <Archiez0077@gmail.com>
2019-03-27 19:43:35 -0500

    Pushed resources for Django shell

--- ScheduleApp/Driver.py
+++ ScheduleApp/Driver.py
@@ -1,5 +1,18 @@
+from django.shortcuts import render
+from django.views import View
+from main.models import YourClass
+# Create your views here.
 
 
-class Driver:
-
-
+class Driver(View):
+    
+  def get(self,request):
+    return render(request, 'main/index.html')
+  def post(self,request):
+    yourInstance = YourClass()
+    commandInput = request.POST["command"]
+    if commandInput:
+      response = yourInstance.command(commandInput)
+    else:
+      response = ""
+    return render(request, 'main/index.html',{"message":response})
