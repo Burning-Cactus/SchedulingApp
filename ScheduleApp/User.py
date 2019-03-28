d24315924257f486a85c0fb2201fd035f2c78f41
real name genij <zgxzgx@gmail.com>
2019-03-27 21:29:22 -0500

    User done besides updateDataBase()

--- ScheduleApp/User.py
+++ ScheduleApp/User.py
@@ -1,5 +1,5 @@
-# test
-
+# Description of User:
+#Made by John and Andy
 
 class User:
     permission = []
@@ -13,33 +13,54 @@ class User:
     officePhone = ""
     extension = ""
 
-    # TODO: Define method stubs for unit testing
-
-    def __methodName__(self):
-        # Format for stubs
-        return 0
-
     #Default Constructor
     def __init__(self):
-
+        #all the variables default to the values above...
         pass
 
     #Parameterized Constructor
-
+    def __init__(self, permission, username, password, databaseID, email, firstName, lastName, contactPhone, officePhone, extension):
+        self.permission=permission
+        self.username=username
+        self.password=password
+        self.databaseID=databaseID
+        self.email=email
+        self.firstName=firstName
+        self.lastName=lastName
+        self.contactPhone=contactPhone
+        self.officePhone=officePhone
+        self.extension=extension
 
     # Create a new User object from a string containing all the fields separated by commas.
-    # The string is formated as: the permission = [],username = "",password = "",databaseID = 0,email = "",firstName = "",lastName = "",contactPhone = "",officePhone = "",extension = ""
-    # Example: ""
-    def fromString(self):
-        # code
+    # The string is formated as: the permission,username,password,databaseID,email,firstName,lastName,contactPhone,officePhone,extension
+    # Example: "(1 2 3),usermane,pass1234,1337,test@gmail.com,first,last,9-(123)-123-1234,1-800-crayonSLAYER,247"
+    #TODO: Require that none of these fields may contain a comma or else this won't work. /n may be a better delimiter
+    def fromString(self, formatedString):
+        params = formatedString.split(",")      #splits the contents of the string into elements of an array
+        params[0] = params[0][1:-1]             #removes the first and last character in (example) "(0 1)" -> "0 1"
+        self.permission = params[0].split(" ")
+        self.username = params[1]
+        self.password = params[2]
+        self.databaseID = params[3]
+        self.email = params[4]
+        self.firstName = params[5]
+        self.lastName = params[6]
+        self.contactPhone = params[7]
+        self.officePhone = params[8]
+        self.extension = params[9]
         return 0
 
     # Returns a string that has all the fields a User has.
     # Currently meant to function with fromString.
     # May want an alternative method for printing out the User in a pretty way.
     def toString(self):
-        pass
+        #can't print an array, grab it's contents
+        intermediate = "("
+        for i in permission:
+            intermediate += i + " "
+        return intermediate+"),"+self.username+","+self.password+","+self.databaseID+","+self.email+","+self.firstName+","+self.lastName+","+self.contactPhone+","+self.officePhone+","+self.extension
 
+    #TODO
     # Update all of User's fields within the database.
     def updateDataBase(self):
         # code
