c68cac98937453879f53130de0942288d7a7cf03
Mike <mesii@uwm.edu>
2019-03-27 20:20:43 -0500

    Auto stash before rebase of "refs/heads/feature/Driver"

--- myApp/views.py
+++ myApp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.views import View
+from main.models import YourClass
 # Create your views here.
+class Home(View):
+  def get(self,request):
+    return render(request, 'main/index.html')
+  def post(self,request):
+    yourInstance = YourClass()
+    commandInput = request.POST["command"]
+    if commandInput:
+      response = yourInstance.command(commandInput)
+    else:
+      response = ""
+    return render(request, 'main/index.html',{"message":response})
