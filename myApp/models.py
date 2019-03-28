f69c1bc6b0e8a63adeb6e5e6836008fc8a833e90
Archie <Archiez0077@gmail.com>
2019-03-27 21:47:33 -0500

    Fleshed out the command stubs

--- myApp/models.py
+++ myApp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from ScheduleApp import User
 
 # Create your models here.
 
@@ -24,46 +25,63 @@ class I_LIST(models.Model):
 
 
 class Terminal:
+    # This class will be used to execute commands with the database.
 
     def command(self, inStr):
         return inStr
 
-    def login(self):
+    def login(self, xName, xPassword):
+        # Create and return a user object from the database after checking the username and password
+        person = User()
+        return person
+
+    def logout(self, xUser):
+        # Destroy the user object currently being used
         return
 
-    def logout(self):
-        return
-
-    def createAccount(self):
+    def createAccount(self, first, last, username, password, email, permission):
+        # Create a new user in the database with all of the parameters provided and a generated ID.
+        person = User()
+        person.toString()
         return""
 
-    def editAccount(self):
+    def editAccount(self, userid):
+        # Change the values of a user in the database.
         return""
 
-    def deleteAccount(self):
+    def deleteAccount(self, userid):
+        # Delete a user model in the database.
         return""
 
-    def createCourse(self):
+    def createCourse(self, name, coursenumber, classnumber, time, location):
+        # Create a course in the database with a generated ID.
         return""
 
-    def email(self):
+    def email(self, message):
+        # Send out an email to notify all recipients.
         return""
 
     def accessData(self):
+        # Access all data in the system and print it in tables
         return""
 
-    def assignInstructorToCourse(self):
+    def assignInstructorToCourse(self, courseid, instructorid):
+        # Assign an instructor to a course in the database
         return""
 
-    def assignAssistantToCourse(self):
+    def assignAssistantToCourse(self, courseid, instructorid):
+        # Assign a TA to a course in the database
         return""
 
-    def viewCourseAssignments(self):
+    def viewCourseAssignments(self, userid):
+        # Return data on instructor assignments in the database.
         return""
 
-    def viewAssistantAssignments(self):
+    def viewAssistantAssignments(self, userid):
+        # Return data on TA assignments in the database
         return""
 
-    def viewContactInfo(self):
+    def viewContactInfo(self, userid):
+        # Return the contact info of the user.
         return""
     # pass
