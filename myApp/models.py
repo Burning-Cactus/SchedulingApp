1c2870a9119696167f40ae692fddf080fca2a248
Archie <Archiez0077@gmail.com>
2019-03-27 20:13:03 -0500

    Worked on models a bit.

--- myApp/models.py
+++ myApp/models.py
@@ -1,17 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
 class USER(models.Model):
     pass
+
 
 class LAB_SECTION(models.Model):
     pass
 
+
 class A_LIST(models.Model):
     pass
+
 
 class COURSE(models.Model):
     pass
 
+
 class I_LIST(models.Model):
     pass
+
+
+class Terminal:
+    def login(self):
+        return
+    # pass
