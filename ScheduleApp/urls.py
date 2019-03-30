6124cc7bc64c0f2a526bb53fb9229386d0896eed
Mike <mesii@uwm.edu>
2019-03-30 16:32:13 -0500

    Tried to fix Cookie issue

--- ScheduleApp/urls.py
+++ ScheduleApp/urls.py
@@ -21,4 +21,5 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^shell/', include('myApp.urls')),
+    path("", include('myApp.urls')),
 ]
--- myApp/views.py
+++ myApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from myApp.models import Terminal
+from django.http import HttpRequest
 # Create your views here.
 class Shell(View):
   # Rocks way
@@ -8,17 +9,17 @@ class Shell(View):
     #return render(request, 'shell/index.html')
 
   def get(self):
-    request = None
+    request = HttpRequest()
     return render(request, 'shell/index.html')
   def post(self,request):
     yourInstance = Terminal()
     commandInput = request.POST["command"]
     if commandInput:
-      response = Terminal.command(commandInput)
+      response = commandInput #Terminal.command(commandInput)
     else:
       response = ""
-    return render(request, 'main/index.html',{"message":response})
+    return render(request, 'shell/index.html',{"message":response})
 
   # prints message to the webpage
   def echo(self, request, message):
-    return render(request, 'main/index.html', {"":message})
+    return render(request,'shell/index.html', {"":message})
