bc534e232279cd6a8a8ff6a6d1e758a6d59eb768
MajorFubar <trolle3.14@gmail.com>
2019-04-03 16:22:39 -0500

    Revert "Merge branch 'feature/Models' of C:\Users\troll\PycharmProjects\SchedulingApp with conflicts."
    This reverts commit 421d502ff4012d8f0aa6319bdffab10e5bc033ac.

--- myApp/models.py
+++ myApp/models.py
@@ -62,11 +62,6 @@ class Terminal:
 
     def createCourse(self, name, coursenumber, classnumber, time, location):
         # Create a course in the database with a generated ID.
-
-        return""
-
-    def deleteCourse(self, coursenumber, classnumber):
-        # Delete a course from the database
         return""
 
     def email(self, message):
