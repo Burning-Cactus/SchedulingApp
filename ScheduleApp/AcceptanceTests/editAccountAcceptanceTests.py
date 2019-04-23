fea5bd110a0e5069034dadd2f7bad988810a1004
MajorFubar <dwensing@uwm.edu>
2019-04-23 14:32:01 -0500

    writing a test

--- ScheduleApp/AcceptanceTests/editAccountAcceptanceTests.py
+++ ScheduleApp/AcceptanceTests/editAccountAcceptanceTests.py
@@ -1,6 +1,10 @@
 from django.test import Client
+from ScheduleApp import User
 
 class AcceptanceTests(Client):
     def editAccountTests(self):
         c = Client()
-        c.post('editAccount.html', {'UserName': 'Boi', 'Password': '~'})
+        c.post('editAccount.html', {'UserName': 'Boi', 'Password': '~', 'Permission': '~', 'Email': '~', 'FirstName':
+                                    '~','LastName':'~', 'ContactPhone': '000-000-0000', 'OfficePhone': '000-000-0000',
+                                    'Extension': '~'})
+        
