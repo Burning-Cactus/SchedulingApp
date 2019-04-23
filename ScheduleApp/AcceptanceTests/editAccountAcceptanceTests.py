b632c6f57a4c0131ce6aac9a861f1fea9fddb60b
MajorFubar <dwensing@uwm.edu>
2019-04-23 17:58:21 -0500

    Made tests that pass and fail

--- ScheduleApp/AcceptanceTests/editAccountAcceptanceTests.py
+++ ScheduleApp/AcceptanceTests/editAccountAcceptanceTests.py
@@ -8,15 +8,22 @@ class AcceptanceTests(TestCase):
                                         firstName="john", lastName="flupper", contactPhone="2628889765",
                                         officePhone="2624235436", extension="151")
 
-
+    def editSelectTests(self):
+        pass
 
     def editAccountTests(self):
         c = Client()
-        go = c.post('editAccount.html', {'UserName': '~', 'Password': '~', 'Permission': '~', 'Email': '~', 'FirstName':
-                                         '~', 'LastName': '~', 'ContactPhone': '000-000-0000', 'OfficePhone':
-                                         '000-000-0000', 'Extension': '~'})
+        idToEdit = self.c.session['editID']
+        ro = self.c.post('editSelect.html', {'UserId': idToEdit})
+        url = ro.redirect_chain
+        self.assertEquals(url, ['editAccount.html'])
+
+        go = self.c.post('editAccount.html', {'UserName': 'Boso', 'Password': '~', 'Permission': '~', 'Email': '~',
+                                              'FirstName': '~', 'LastName': '~', 'ContactPhone': '000-000-0000',
+                                              'OfficePhone': '000-000-0000', 'Extension': '~'})
         url = go.redirect_chain
         self.assertEquals(url, ['http://127.0.0.1:8000/home'])
 
-        self.assertEqual(USER.objects.get(id=self.user.id), )
+        self.assertEqual(c.session['editID'].username, 'Boso')
 
+        self.assertEqual(c.session['editID'].username, 'Mozo')
