1ef124b85363537dd2af32c1a068d7409b660852
ArcboundWorker <ananan333>
2019-04-23 14:39:02 -0500

    Misc. bug fixes.

--- ScheduleApp/AcceptanceTests/deleteAccountHttpAcceptanceTests.py
+++ ScheduleApp/AcceptanceTests/deleteAccountHttpAcceptanceTests.py
@@ -1,18 +1,20 @@
 from django.test import Client, TestCase
+from django.urls import reverse
+from . import views
 
-class deleteAccountHttpAcceptanceTest():
 
-    #Tests a properly used delete page
-        #returns a status_code equal to 200
+class DeleteAccountHttpAcceptanceTest(TestCase):
+
+    # Tests a properly used delete page
+        # returns a status_code equal to 200
     def test1(self):
         c = Client()
         response = c.post('/delete/', {'username': 'john', 'password': 'smith'})
         response = c.get('/customer/details/')
         TestCase.assertEqual(response.status_code, 200)
 
-        #check databaseID
-        #looking at session and make sure it is equal to something?
+        # check databaseID
+        # looking at session and make sure it is equal to something?
 
-        TestCase.assertEqual()
 
 
