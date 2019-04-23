7ace16e86f46df0fec2c0c5bd095a4db912f5389
MajorFubar <dwensing@uwm.edu>
2019-04-23 14:45:31 -0500

    pushing so I can pull

--- ScheduleApp/AcceptanceTests/editAccountAcceptanceTests.py
+++ ScheduleApp/AcceptanceTests/editAccountAcceptanceTests.py
@@ -1,10 +1,12 @@
-from django.test import Client
+from django.test import Client, TestCase
 from ScheduleApp import User
 
-class AcceptanceTests(Client):
+class AcceptanceTests(TestCase):
     def editAccountTests(self):
         c = Client()
+        newUser = User
         c.post('editAccount.html', {'UserName': 'Boi', 'Password': '~', 'Permission': '~', 'Email': '~', 'FirstName':
-                                    '~','LastName':'~', 'ContactPhone': '000-000-0000', 'OfficePhone': '000-000-0000',
+                                    '~', 'LastName': '~', 'ContactPhone': '000-000-0000', 'OfficePhone': '000-000-0000',
                                     'Extension': '~'})
-        
+
+
