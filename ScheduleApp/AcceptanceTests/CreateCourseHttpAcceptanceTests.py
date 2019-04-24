c0a3aecfbcdedb66e262f0bf8a4a8bfda7979f46
Archie <Archiez0077@gmail.com>
2019-04-24 16:46:39 -0500

    Update CreateCourseHttpAcceptanceTests.py

--- ScheduleApp/AcceptanceTests/CreateCourseHttpAcceptanceTests.py
+++ ScheduleApp/AcceptanceTests/CreateCourseHttpAcceptanceTests.py
@@ -6,7 +6,7 @@ from myApp.models import USER
 class CreateCourseTest(TestCase):
     
     def setup(self):
-        self.user = USER.objects.create(permission=[4], username="john", password="test", email="john@this.com",
+        self.user = USER.objects.create(permission=[1], username="john", password="test", email="john@this.com",
                                         firstName="john", lastName="flupper", contactPhone="2628889765",
                                         officePhone="2624235436", extension="151")
     
@@ -20,7 +20,9 @@ class CreateCourseTest(TestCase):
         response = c.post('/createCourse/')
         url = response.redirect_chain
         self.assertEquals(url, '/createCourse/')
-       
+    
+    # Test the functionality of creating a course
     def testFunc(self):
         c = Client()
-        
+        response = c.post('/createCourse/', {'subject': 'Smash', 'coursenumber': '101', 'classnumber': '001', 'time': '10:00-10:50 TR', 'place': 'Lubar S263'})
+        self.assertEquals(response.status_code, 200)
