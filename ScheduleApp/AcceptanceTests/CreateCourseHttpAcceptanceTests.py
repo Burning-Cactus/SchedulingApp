3b9de948bc34326abf0f7e54571e416b63ef69b9
Archie <Archiez0077@gmail.com>
2019-04-24 16:18:58 -0500

    Update CreateCourseHttpAcceptanceTests.py

--- ScheduleApp/AcceptanceTests/CreateCourseHttpAcceptanceTests.py
+++ ScheduleApp/AcceptanceTests/CreateCourseHttpAcceptanceTests.py
@@ -10,9 +10,17 @@ class CreateCourseTest(TestCase):
                                         firstName="john", lastName="flupper", contactPhone="2628889765",
                                         officePhone="2624235436", extension="151")
     
-    def testurl(self):    
+    # Test to make sure the url is set correctly
+    def testUrl(self):    
         c = Client()
-        response = c.post('/login/', {'username': 'john', 'password': 'smith'})
-        response.status_code
-        response = c.get('/createCourse/')
-        response.content
+        # response = c.post('/login/', {'username': 'john', 'password': 'smith'})
+        # response.status_code
+        # response = c.get('/createCourse/')
+        # response.content
+        response = c.post('/createCourse/')
+        url = response.redirect_chain
+        self.assertEquals(url, '/createCourse/')
+       
+    def testFunc(self):
+        c = Client()
+        
