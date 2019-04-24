dace04d23943a5ccc121cc071ab245635eecfbf7
Archie <Archiez0077@gmail.com>
2019-04-24 16:04:43 -0500

    Update CreateCourseHttpAcceptanceTests.py

--- ScheduleApp/AcceptanceTests/CreateCourseHttpAcceptanceTests.py
+++ ScheduleApp/AcceptanceTests/CreateCourseHttpAcceptanceTests.py
@@ -1,9 +1,18 @@
 from django.test import Client
 from django.test import TestCase
+from myApp.models import USER
+
 
 class CreateCourseTest(TestCase):
-    c = Client()
-    response = c.post('/login/', {'username': 'john', 'password': 'smith'})
-    response.status_code
-    response = c.get('/createCourse/')
-    response.content
+    
+    def setup(self):
+        self.user = USER.objects.create(permission=[4], username="john", password="test", email="john@this.com",
+                                        firstName="john", lastName="flupper", contactPhone="2628889765",
+                                        officePhone="2624235436", extension="151")
+    
+    def testurl(self):    
+        c = Client()
+        response = c.post('/login/', {'username': 'john', 'password': 'smith'})
+        response.status_code
+        response = c.get('/createCourse/')
+        response.content
