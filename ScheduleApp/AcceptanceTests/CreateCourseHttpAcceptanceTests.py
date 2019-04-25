9aab0e7c98007a643fee5a63cc8ed7bd644a1b8c
Archie <Archiez0077@gmail.com>
2019-04-24 20:04:49 -0500

    Pushing what I have for this

--- ScheduleApp/AcceptanceTests/CreateCourseHttpAcceptanceTests.py
+++ ScheduleApp/AcceptanceTests/CreateCourseHttpAcceptanceTests.py
@@ -1,9 +1,22 @@
 from django.test import Client
 from django.test import TestCase
+from myApp.models import USER
+from myApp.models import COURSE
+
 
 class CreateCourseTest(TestCase):
-    c = Client()
-    response = c.post('/login/', {'username': 'john', 'password': 'smith'})
-    response.status_code
-    response = c.get('/createCourse/')
-    response.content
+
+    def setup(self):
+        self.user = USER.objects.create(permission=[4], username="john", password="test", email="john@this.com",
+                                        firstName="john", lastName="flupper", contactPhone="2628889765",
+                                        officePhone="2624235436", extension="151")
+
+    def testurl(self):
+        c = Client()
+
+    def test1(self):
+        c = Client()
+        response = c.post('/login/', {'username': 'john', 'password': 'smith'})
+        response.status_code
+        response = c.get('/createCourse/')
+        response.content
