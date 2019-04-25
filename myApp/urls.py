14bcfdc83e28d46078c8aa66e47f54d76772576f
Mitchell Kluge <mjkluge@uwm.edu>
2019-04-24 22:09:02 -0500

    commands is most certainty something

--- myApp/urls.py
+++ myApp/urls.py
@@ -7,5 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("", views.Shell.as_view()),
 path('createAccount/', views.createAccount.as_view()),
-path('createAccountError/', views.createAccountError.as_view())
+path('createAccountError/', views.createAccountError.as_view()),
+path('commands/', views.commands.as_view()),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
